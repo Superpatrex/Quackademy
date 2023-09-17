c12c7ecbbdb61b0037ef69c8570cb9079786e48e
AugustDruzgal <august.druzgal@Knights.ucf.edu>
2023-09-17 01:29:47 -0400

    new view
    new view

--- Assets/Scripts/CameraView.cs
+++ Assets/Scripts/CameraView.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class CameraView : MonoBehaviour
 {
     public Camera OverheadCamera;
+    public Camera FloorCamera;
     public Camera SpectatorCamera;
     public GameObject TeacherMenu;
 
     private Camera CurrentCamera;
-    private float rotation = 0;
+    private float oRotation = -90;
+    private float fRotation = -90;
 
     // Start is called before the first frame update
     void Start()
@@ -24,14 +26,21 @@ public class CameraView : MonoBehaviour
         if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha1))
         {
             CurrentCamera.enabled = false;
-            CurrentCamera = OverheadCamera;
+            CurrentCamera = SpectatorCamera;
             CurrentCamera.enabled = true;
         }
 
         if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha2))
         {
             CurrentCamera.enabled = false;
-            CurrentCamera = SpectatorCamera;
+            CurrentCamera = OverheadCamera;
+            CurrentCamera.enabled = true;
+        }
+
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CurrentCamera.enabled = false;
+            CurrentCamera = FloorCamera;
             CurrentCamera.enabled = true;
         }
 
@@ -42,16 +51,36 @@ public class CameraView : MonoBehaviour
         {
             if (UnityEngine.Input.GetKey(KeyCode.LeftArrow))
             {
-                rotation += 45f * (Time.deltaTime);
+                oRotation += 45f * (Time.deltaTime);
             }
 
             if (UnityEngine.Input.GetKey(KeyCode.RightArrow))
             {
-                rotation -= 45f * (Time.deltaTime);
+                oRotation -= 45f * (Time.deltaTime);
             }
 
-            OverheadCamera.transform.position = Quaternion.Euler(0, rotation, 0) * new Vector3(0, 5.2f, -5.5f);
+            OverheadCamera.transform.position = Quaternion.Euler(0, oRotation, 0) * new Vector3(0, 5.2f, -5.5f);
             OverheadCamera.transform.rotation = Quaternion.LookRotation(SpectatorCamera.transform.position - OverheadCamera.transform.position);
+        }
+
+        if (FloorCamera.enabled)
+        {
+            if (UnityEngine.Input.GetKey(KeyCode.LeftArrow))
+            {
+                fRotation += 45f * (Time.deltaTime);
+                if (fRotation > 0)
+                    fRotation = 0;
+            }
+
+            if (UnityEngine.Input.GetKey(KeyCode.RightArrow))
+            {
+                fRotation -= 45f * (Time.deltaTime);
+                if (fRotation < -180)
+                    fRotation = -180;
+            }
+
+            FloorCamera.transform.position = Quaternion.Euler(0, fRotation, 0) * new Vector3(0, 1.5f, -3.5f);
+            FloorCamera.transform.rotation = Quaternion.LookRotation(SpectatorCamera.transform.position - FloorCamera.transform.position + new Vector3(-2, 0, 0));
         }
 
         TeacherMenu.transform.position = CurrentCamera.transform.position + CurrentCamera.transform.forward * 1;
